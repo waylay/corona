97a97b4215bf940e1d0166e04cacfc5242aa0beb
Cristian Ionel <cristian.ionel@gmail.com>
2018-07-20 00:26:44 +0300

    Parallax

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -36,8 +36,36 @@ jQuery.fn.forceNumeric = function () {
     });
 }
 
+function isInViewport(node) {
+    var rect = node.getBoundingClientRect()
+    return (
+        (rect.height > 0 || rect.width > 0) &&
+        rect.bottom >= 0 &&
+        rect.right >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+}
 
 $( document ).ready( function () {
+
+    // Parallax effect
+    $(window).scroll(function() {
+        var scrolled = $(window).scrollTop()
+        $('.slider-image').each(function(index, element) {
+            var initY = $(this).offset().top
+            var height = $(this).height()
+            var endY  = initY + $(this).height()
+        
+            // Check if the element is in the viewport.
+            var visible = isInViewport(this)
+            if(visible && $(window).width() > 767) {
+                var diff = scrolled - initY
+                var ratio = Math.round((diff / height) * 100)
+                $(this).css('background-position','center ' + parseInt(+(ratio * 1.5 + 15)) + 'px')
+            }
+        })
+    })
     
     // Event reminder dropdown
     var festival = $('.menu-reminder');
