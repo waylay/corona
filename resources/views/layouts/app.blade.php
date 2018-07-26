b7f0d82575fa8f0314615ee7b62ac6b225699588
waylay <cristian.ionel@gmail.com>
2018-07-25 21:39:03 -0700

    GA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,24 @@
     <!-- End Facebook Pixel Code -->
     
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122916897-1"></script>
+
+<script>
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+ 
+
+  gtag('config', 'UA-122916897-1');
+
+</script>
+
 </head>
 
 <body class="{{ Route::currentRouteName() }} lang-{{ app()->getLocale() }}">
